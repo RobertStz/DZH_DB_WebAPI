17240555cdccdd13f0e1ee250f6c3db4066a2625
Josh <JoshuaMirow@web.de>
2022-07-02 11:20:18 +0200

    Login

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {CitizenService} from "../shared/Services/citizen.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private user: CitizenService) { }
 
   ngOnInit(): void {
+  }
+
+  login($event: MouseEvent): void{
+
+    this.user.logInCitizen((document.getElementById('') as HTMLInputElement).value,(document.getElementById('') as HTMLInputElement).value).subscribe( data =>{
+      console.log(data)
+    }, (err: HttpErrorResponse) =>{
+      switch (err.status){
+        case 500:
+          alert("Anmeldung momentan nicht verfügbar");
+          break;
+        case 403:
+          alert("Passwort oder E-Mail falsch");
+          break;
+        case 404:
+          alert("Konto existiert noch nicht! \n Bitte Registrieren Sie sich zuerst ");
+          break;
+      }
+    });
   }
 
 }
