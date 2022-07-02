2e14aaf1102effac7b04266e0499b3541c760d11
Josh <JoshuaMirow@web.de>
2022-07-02 09:01:35 +0200

    Anlegen der weiteren Komponenten

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import {AuthServiceGuard} from "./shared/Guards/auth-service.guard";
 import {LoginService} from "./shared/Services/login.service";
+import { AppLayoutComponent } from './app-layout/app-layout.component';
+import { StartseiteComponent } from './startseite/startseite.component';
+import { ProfilComponent } from './profil/profil.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 const myRoutes: Routes = [
 
@@ -22,7 +27,12 @@ const myRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    AppLayoutComponent,
+    StartseiteComponent,
+    ProfilComponent,
+    TasksComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
