72fb508f9342f1551e810fee5152c6c3035cd0bc
Josh <JoshuaMirow@web.de>
2022-07-04 11:38:48 +0200

    LogOut angepasst

--- Frontend/src/app/header/header.component.ts
+++ Frontend/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import {LoginService} from "../shared/login.service";
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,7 @@ import {Router} from "@angular/router";
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private log: LoginService) { }
 
   ngOnInit(): void {
   }
@@ -32,6 +33,7 @@ export class HeaderComponent implements OnInit {
   }
 
   logOut($event: MouseEvent){
-    this.router.navigate([''])
+    this.log.myStorage.removeItem('key');
+    this.router.navigate(['']);
   }
 }
