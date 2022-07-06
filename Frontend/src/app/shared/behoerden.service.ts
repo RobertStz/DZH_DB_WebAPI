2edd639361e1b084ae707ad19c1cbd7f0ea92798
Josh <JoshuaMirow@web.de>
2022-07-06 14:14:33 +0200

    Change

--- Frontend/src/app/shared/behoerden.service.ts
+++ Frontend/src/app/shared/behoerden.service.ts
@@ -20,14 +20,14 @@ export class BehoerdenService {
     return this.http.post<Authority>(this.url,behoerde, {headers: this.httpHeader});
   }
 
-  public getAuthorityProcesses(id: number):Observable<Process>{
-    return this.http.get<Process>(this.url+"/"+id+"/processes");
+  public getAuthorityProcesses(id: number):Observable<Process[]>{
+    return this.http.get<Process[]>(this.url+"/"+id+"/processes");
   }
-  public getAuthorityMessages(id: number):Observable<Message>{
-    return this.http.get<Message>(this.url+"/"+id+"/messages");
+  public getAuthorityMessages(id: number):Observable<Message[]>{
+    return this.http.get<Message[]>(this.url+"/"+id+"/messages");
   }
-  public getAuthorityCitizen(id: number):Observable<Citizen>{
-    return this.http.get<Citizen>(this.url+"/"+id+"/citizen");
+  public getAuthorityCitizen(id: number):Observable<Citizen[]>{
+    return this.http.get<Citizen[]>(this.url+"/"+id+"/citizen");
   }
   public updateAuthority(data: Authority):Observable<boolean>{
     return this.http.put<boolean>(this.url+"/"+data.id, data,{headers: this.httpHeader});
